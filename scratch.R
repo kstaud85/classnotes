a68f4c91b4a1d40f6279ebfaea33c7d789db56e4
One <One@L09-mainll.Oldmainlab.local>
2017-10-02 12:44:09 -0400

    version1

---
+++ scratch.R
@@ -0,0 +1,20 @@
+deaths<-read.csv('KoreanConflict.csv',header=TRUE,stringsAsFactors = FALSE)
+#run, colnames(deaths), deaths$INCIDENT_DATE
+#str_detect("19931211","^\\d{8}$") <-8 digits, date, must have \\, ^ look for that at beinging, $ at the end (8digits)
+
+str_detect(deaths$INCIDENT_DATE,"\\d{8}")
+
+sum(str_detect(deaths$INCIDENT_DATE,"\\d{8}"))
+dim(deaths) #how many rows and columns there are, subtract dim from sum to find number of mistakes
+
+#loop: ask if theres a mistake, and if the date is out of place
+
+#[i]looking at i incident in column to see if theres a pattern 
+for(i in 1:36574){ 
+  incident<-str_detect(deaths$INCIDENT_DATE[i],"\\d{8}")
+  fatality<-str_detect(deaths$FATALITY[i],"\\d{8}")
+  if(incident==FALSE & fatality==TRUE){
+    deaths$INCIDENT_DATE[i]<-deaths$FATALITY[i]
+  }
+  print(i)
+}
